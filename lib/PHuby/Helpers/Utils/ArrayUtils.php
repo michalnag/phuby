0f05adbdbab28f4eed0680c9fb3897953e032e66
MichalN <nagielll@gmail.com>
2017-01-10 12:23:49 +0000

    Added more functionality in ImageUtils

--- lib/PHuby/Helpers/Utils/ArrayUtils.php
+++ lib/PHuby/Helpers/Utils/ArrayUtils.php
@@ -9,7 +9,7 @@ use PHuby\Logger;
 class ArrayUtils extends AbstractUtils {
    
 
-  public static function check_if_multiple_keys_exist(Array $arr_keys, Array $arr_haystack) {
+  public static function check_multiple_keys(Array $arr_keys, Array $arr_haystack) {
     $bol_all_keys_exist = true;
     foreach($arr_keys as $key) {
       if(!array_key_exists($key, $arr_haystack)) {
--- lib/PHuby/Helpers/Utils/ImageUtils.php
+++ lib/PHuby/Helpers/Utils/ImageUtils.php
@@ -4,13 +4,18 @@ namespace PHuby\Helpers\Utils;
 
 use PHuby\Helpers\AbstractUtils;
 use PHuby\Error\FileError;
+use PHuby\Error\MissingParameterError;
+use PHuby\Helpers\Utils\ArrayUtils;
 
 class ImageUtils extends AbstractUtils implements FileTypeInterface {
 
-  const ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png'];
-  const DPI_OPTIONS = [
-    72, 96, 120, 160, 200, 240, 300
-  ];
+  const 
+    ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png'],
+    DPI_OPTIONS = [
+      72, 96, 120, 160, 200, 240, 300
+    ],
+    DEFAULT_IMG_QUALITY = 100;
+
 
   // 1 dpi = 0.393701 pixel/cm;
   const ONE_DPI_PX_BY_CM_RATIO = 0.393701;
@@ -90,60 +95,198 @@ class ImageUtils extends AbstractUtils implements FileTypeInterface {
     return self::DPI_OPTIONS[0];
   }
 
-  public static function resize($str_img_path, $int_max_width, $int_max_height) {
-    
-    $picture = $str_img_path;
-    $picture_details    = getimagesize($picture);
+  /**
+   * Method calculates new size of the image based on given parameters.
+   * It keeps the ratio of the old parameters, and uses new dimensions as thresholds
+   * 
+   * array['current_width']     int containing current width in px
+   * array['current_height']    int containing current height in px
+   * array['max_width']         int containing new max width in px
+   * array['max_height']        int containing new max height in px
+   * 
+   * @param mixed[] array $arr_params (see above)
+   * @return mixed[] containing new dimensions, with keys height and width
+   * @throws \PHuby\Error\MissingParameterError if any of the required parameter is misisng
+   */
+  public static function calculate_new_size(Array $arr_params) {
+    $arr_required_params = ['current_width', 'current_height', 'max_width', 'max_height'];
+    if(ArrayUtils::check_multiple_keys($arr_required_params, $arr_params)) {
 
-        $width  = $picture_details[0];
-        $height = $picture_details[1];
-        
-        // E.G. 200 / 100 = 2
-        $ratio = $width/$height;
+      // Assign passed parameters to variables
+      foreach($arr_required_params as $param => $value) {
+        $$param = $value;
+      }
 
+      // Calculate ratio
+      $ratio = $current_width/$current_height;
 
-        // Check width
-        if($max_w >= $width) {
-            // Width is ok. Check height
-            if($max_h >= $height) {
-                // Height is ok too
-                $dimensions = array($width, $height);
-            } else {
-                // Picture too high. Adjust based on ratio
-                // e.g. New height = 50, width = height * ratio
-                $dimensions = array(round($max_h*$ratio), $max_h);
-            }
+      // Start calculating new dimensions
+      // Check width
+      if($max_width >= $current_width) {
+        // Width is ok. Check height
+        if($max_height >= $current_height) {
+          // Height is ok too
+          $dimensions = array($current_width, $current_height);
         } else {
-            // Picture too wide
-            if($max_h >= $height) {
-                // Height is ok. adjust based on width.
-                // e.g. width = 100, height = width / ratio
-                $dimensions = array($max_w, round($max_w/$ratio));
-            } else {
-                // Both height and width are too big
-                // Get new dimensions ratio
-                $max_ratio = $max_w / $max_h;
-            
-                if($max_ratio < $ratio) {
-                    // We need to adjust dimensions based on width
-                    $dimensions = array($max_w, $max_w/$ratio);
-                } elseif($max_ratio == $ratio) {
-                    // Assign max values
-                    $dimensions = array($max_w, $max_h);
-                } else {
-                    // Adjust based on height
-                    $dimensions = array($max_h*$ratio, $max_h);
-                }
-            }
+          // Picture too high. Adjust based on ratio
+          // e.g. New height = 50, width = height * ratio
+          $dimensions = array(round($max_height*$ratio), $max_height);
+        }
+      } else {
+        // Picture too wide
+        if($max_height >= $current_height) {
+          // Height is ok. adjust based on width.
+          // e.g. width = 100, height = width / ratio
+          $dimensions = array($max_width, round($max_width/$ratio));
+        } else {
+          // Both height and width are too big
+          // Get new dimensions ratio
+          $max_ratio = $max_width / $max_height;
+      
+          if($max_ratio < $ratio) {
+            // We need to adjust dimensions based on width
+            $dimensions = array($max_width, $max_width/$ratio);
+          } elseif($max_ratio == $ratio) {
+            // Assign max values
+            $dimensions = array($max_width, $max_height);
+          } else {
+            // Adjust based on height
+            $dimensions = array($max_height*$ratio, $max_height);
+          }
+        }
+      }
+
+      // Once calculated, return new values as an array
+      return [
+        "width" => $dimensions[0],
+        "height" => $dimensions[1]
+      ];
+
+    } else {
+      throw new MissingParameterError("Unable to calculate new image size, as some parameters are missing. Got " . json_encode($arr_params));
+    }
+  }
+
+
+  /**
+   * Method resizes the image and makes a permanent change to it
+   * 
+   * array['image_path']        string containing absolute path to the image
+   * array['max_width']         int containing new max width in px
+   * array['max_height']        int containing new max height in px
+   * array['quality']           int (optional) containing quality of the image (1 to 100)
+   * 
+   * @param mixed[] array $arr_params (see above)
+   * @return boolean true if image has been resized
+   * @throws \PHuby\Error\FileError if image is not readable
+   * @throws \PHuby\Error\FileError if unable to resize image
+   * @throws \PHuby\Error\MissingParameterError if any of the required parameter is misisng
+   */
+  public static function resize(Array $arr_params) {
+    $arr_required_params = ["image_path", "max_width", "max_height"];
+    if(ArrayUtils::check_multiple_keys($arr_required_params, $arr_params)) {
+
+      // Now check if the file exists and is readable
+      if(FileUtils::is_readable($arr_params["image_path"])) {
+
+        // Get current height and width of the picture
+        list($current_width, $current_height)    = getimagesize($arr_params["image_path"]);
+        
+        list($new_width, $new_height) = self::calculate_new_size([
+            "current_width" => $current_width,
+            "current_height" => $current_height,
+            "max_width" => $int_max_width,
+            "max_height" => $int_max_height
+          ]);
+
+        if(array_key_exists("quality", $arr_params)) {
+          $quality = $arr_params["quality"];
+        } else {
+          $quality = self::DEFAULT_IMG_QUALITY;
         }
 
-        list($new_width, $new_height) = $dimensions;
+        // Once calculated, resize and save the image
+        $image_copy = imagecreatefromjpeg($arr_params["image_path"]);
+        $new_image  = imagecreatetruecolor($new_width, $new_height);
+        imagecopyresampled($new_image, $image_copy, 0, 0, 0, 0, $new_width, $new_height, $current_width, $current_height);
 
-        if($this->resize_and_save($new_width, $new_height, $width, $height, $picture)) {
-            return true;
+        if(imagejpeg($new_image, $arr_params["image_path"], 100)) {
+          // Image has been resized correctly
+          return true;
         } else {
-            return false;
-        } 
+          throw new FileError("Unable to resize the image.");
+        }
+
+      } else {
+        throw new FileError("File located in " . $arr_params["image_path"] . " is not readable");
+      }
+
+    } else {
+      throw new MissingParameterError("Unable to resize image as method hasn't received all required parameters. Got: " . json_encode($arr_params). ". Require: " . json_encode($arr_required_params));
+    }
+  }
+
+  /**
+   * Method crops the picture using different source locations.
+   * 
+   * array['image_path']        string containing absolute path to the image
+   * array['source_x']          int containing x px location to crop from
+   * array['source_y']          int containing y px location to crop from
+   * array['target_width']      int containing target width in px
+   * array['target_height']     int containing target height in px
+   * array['source_width']      int containing width in px of the source image
+   * array['source_height']     int containing height in px of the source image
+   * array['quality']           int (optional) containing quality of the image (1 to 100)
+   * 
+   * @param mixed[] array $arr_params (see above)
+   * @return boolean true if picture has been cropped properly
+   * @throws \PHuby\Error\MissingParameterError if any of the required parameter is misisng
+   * @throws \PHuby\Error\FileError if image cannot be cropped
+   * @throws \PHuby\Error\FileError if image is not readable
+   */
+  public static function crop(Array $arr_params) {
+    $arr_required_params = ["image_path", "source_x", "source_y", "target_width", "target_height"];
+    if(ArrayUtils::check_multiple_keys($arr_required_params, $arr_params)) {
+
+      // Check if the image is readeable
+      if(FileUtils::is_readable($arr_params["image_path"])) {
+
+        // In case cropping takes place
+        $source_image = imagecreatefromjpeg($arr_params["image_path"]);
+        $destination_image = imagecreatetruecolor($arr_params["target_width"], $arr_params["target_height"]);
+
+        if(array_key_exists("quality", $arr_params)) {
+          $quality = $arr_params["quality"];
+        } else {
+          $quality = self::DEFAULT_IMG_QUALITY;
+        }
+
+        imagecopyresampled(
+          $destination_image,
+          $source_image,
+          0,
+          0,
+          $arr_params["source_x"],
+          $arr_params["source_y"],
+          $arr_params["target_width"],
+          $arr_params["target_height"],
+          $arr_params["source_width"],
+          $arr_params["source_height"]
+        );
+
+        if(imagejpeg($destination_image, $source_image ,$quality)) {
+          return true;
+        } else {
+          throw new FileError("Unable to crop the file.");
+        }
+
+      } else {
+        throw new FileError("File located in " . $arr_params["image_path"] . " is not readable");
+      }   
+
+    } else {
+      throw new MissingParameterError("Unable to resize image as method hasn't received all required parameters. Got: " . json_encode($arr_params). ". Require: " . json_encode($arr_required_params));     
+    }
   }
 
 }
