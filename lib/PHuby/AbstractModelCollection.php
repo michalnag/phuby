58e91b13670c9b4e749a1f47e471e421073e096b
MichalN <nagielll@gmail.com>
2017-01-13 13:31:51 +0000

    Improved interanals of ModelCollection class

--- lib/PHuby/AbstractModelCollection.php
+++ lib/PHuby/AbstractModelCollection.php
@@ -14,10 +14,50 @@ abstract class AbstractModelCollection extends AbstractModel implements BaseMode
     // Collection method expects an array of arrays so let's check if the right format has been passed
     foreach ($array as $data) {
       // Create an instance of the Collectable class
-      $class_name = $this::COLLECT_CLASS['name'];
-      $collectable_object = new $class_name();
-      $collectable_object->populate_attributes($data);
-      $this->collection[] = $collectable_object;
+      $this->add_to_collection($this->array_to_collactable_object($data));
+    }
+  }
+
+  protected function array_to_collactable_object(Array $arr_data) {
+    $class_name = $this::COLLECT_CLASS['name'];
+    $obj_collectable = new $class_name();
+    $obj_collectable->populate_attributes($arr_data);
+    return $obj_collectable;
+  }
+
+  public function add_to_collection($obj_collectable) {
+    $str_collect_class = $this::COLLECT_CLASS['name'];
+    if($obj_collectable instanceof $str_collect_class) {
+      $this->collection[] = $obj_collectable;
+    } else {
+      throw new Error\InvalidArgumentError(get_class($this) . " expects an instace of $str_collect_class, got " . get_class($obj_collectable));
+    }
+  }
+
+  public function is_collection_populated() {
+    return !empty($this->collection);
+  }
+
+  /**
+   * Restarts collection to an empty array
+   * 
+   * @return boolean true once collection is cleared
+   */
+  public function empty_collection() {
+    $this->collection = [];
+    return true;
+  }
+
+  /**
+   * Allows to pass an array with data representing a collactable object data
+   * creates respective class and adds it to the collection.
+   * 
+   * @param mixed[] $arr_data Array representing the data to be put to the collection
+   * @return boolean true if data is added to the collection
+   */
+  public function add_raw_to_collection(Array $arr_data) {
+    foreach($arr_data as $key => $value) {
+
     }
   }
 
