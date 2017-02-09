baa357924a78f50cdd6b68380d94baa414dff623
MichalN <nagielll@gmail.com>
2017-02-09 08:35:32 +0000

    Attribute initiation completed

--- lib/PHuby/AbstractCore.php
+++ lib/PHuby/AbstractCore.php
@@ -21,6 +21,7 @@ abstract class AbstractCore {
    */
   public function set_attr($str_attr_name, $value) {}
 
+
   /**
    * Magic method gets the attribute set on the object based on ATTRIBUTE_MAP
    * @param string $str_attr_name representing name of the attribute
@@ -28,13 +29,38 @@ abstract class AbstractCore {
    */
   public function &get_attr($str_attr_name) {}
 
+
   /**
    * Method initiates attributes based on the ATTRIBUTE_MAP array configured on
    * the child class, attempting to set a default value, or null if not set.
    * This method can be run on the __contruct but only in the child class.
-   * @return boolean true once all attributes are created
+   * @return boolean true once all attributes are created, false if ATTRIBUTE_MAP is undefined
    */
-  public function initiate_attributes() {}
+  public function initiate_attributes() {
+
+    // Check if the ATTRIBUTE_MAP is defined
+    $str_caller_class = get_class($this);
+    if (defined("$str_caller_class::ATTRIBUTE_MAP")) {
+
+      // We have a map set. Iterate over it and create standard attributes
+      foreach ($this::ATTRIBUTE_MAP as $str_attr_name => $arr_attr_details) {
+        // Check if this is standard attribute
+        if ($this->is_standard_attribute($str_attr_name)) {
+          // Standard attribute. Instantiate it
+          $str_attr_class = $arr_attr_details['class'];
+          $this->$str_attr_name = new $str_attr_class;
+        }
+      }
+
+      // Process completed. Return true
+      return true;
+
+    } else {
+      // No ATTRIBUTE_MAP set. Return false
+      return false;
+    }
+  }
+
 
   /**
    * Method for populating attributes based on the ATTRIBUTE_MAP array
@@ -42,5 +68,16 @@ abstract class AbstractCore {
    * @param mixed[] $arr_attributes containing data to be populated
    */
   public function populate_attributes(Array $arr_attributes) {}
+
+
+  /**
+   * Method checks is the attribute is a standard attribute and not a child class
+   * @param string $str_attr_name representing parameter to be checked
+   * @return boolean true if it is a standard attribute, false otherwise
+   */
+  public function is_standard_attribute($str_attribute_name) {
+    $caller_class = get_class($this);
+    return defined("$caller_class::ATTRIBUTE_MAP") && array_key_exists($str_attribute_name, $this::ATTRIBUTE_MAP) && array_key_exists("class", $this::ATTRIBUTE_MAP[$str_attribute_name]);
+  }
 
 }
