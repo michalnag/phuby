1838c05b923e3f7ae0039c19405bb68b6bf5d979
nagiel <nagielll@gmail.com>
2017-02-15 14:48:03 +0000

    Added more collection methods

--- lib/PHuby/AbstractCollection.php
+++ lib/PHuby/AbstractCollection.php
@@ -8,6 +8,8 @@
 
 namespace PHuby;
 
+use PHuby\Error;
+
 abstract class AbstractCollection extends AbstractCore {
 
   protected $collection = [];
@@ -16,18 +18,35 @@ abstract class AbstractCollection extends AbstractCore {
    * Method populates the collection on the object, if the object is a collection class.
    * It relies on COLLECT_CLASS constant to be present on caller class, which represents the class to collect.
    * @param mixed[] $arr_collection containg array of arrays|objects representing the collection
-   * @return boolean true when collection has finished populating
+   *    (null is acceptable)
+   * @return boolean true when collection has finished populating, false otherwise
+   * @throws PHuby\Error\InvalidArgumentError when invalid argument is passed
    * @todo - check if the COLLECT_CLASS is defined
+   * @todo - check if array is empty
    */
-  public function populate_collection(Array $arr_collection) {
-  
-    // Iterate over the collection and try to add all elements ti the collection
-    foreach ($arr_collection as $data) {
+  public function populate_collection($arr_collection) {
+    
+    // Check if correct parameter has been passed
+    if (is_array($arr_collection)) { 
+        
+      // Iterate over the collection and try to add all elements ti the collection
+      foreach ($arr_collection as $data) {
 
-      // Simply add to the collection
-      $this->add_to_collection($data);
+        // Simply add to the collection
+        $this->add_to_collection($data);
 
+      }
+
+    } elseif(is_null($arr_collection)) {
+
+      // No collection has been popualted because null has been passed
+      return false;
+
+    } else {
+      // Invalid argument passed
+      throw new Error\InvalidArgumentError(__METHOD__ . " must be passed an array or null. Got " . gettype($arr_collection));
     }
+
 
     // We finished the loop and popualting collection
     return true;
@@ -105,6 +124,39 @@ abstract class AbstractCollection extends AbstractCore {
       return $arr_raw_data;
     } else {
       return null;
+    }
+  }
+
+
+  /**
+   * Method will populate same attribute to all objects inside the collection
+   * @param mixed[] $arr_attributes Array conatining attributes names and values
+   * @todo - build and complete this method
+   */
+  public function populate_attributes_to_collection(Array $arr_attributes) {}
+
+
+  /**
+   * Method sets a single attribute on all objects inside the collection 
+   * @param string $str_attr_name representing a name of the attribute
+   * @param mixed $attr_value representing an attribute value
+   * @return boolean true when succesfully set, false if collection is not populated
+   */
+  public function set_attr_on_collection($str_attr_name, $attr_value) {
+
+    // Check if collection is populated first
+    if($this->is_collection_populated()) {
+      
+      foreach($this->collection as $obj_collectable) {
+        $obj_collectable->set_attr($str_attr_name, $attr_value);
+      }
+
+      // Setting completed
+      return true;
+
+    } else {
+      // Collection is not populated
+      return false;
     }
   }
 
