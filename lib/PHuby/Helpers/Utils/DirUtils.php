f46cd4ac87b99fb844d59137ba149935a4e36c00
Nagiel <nagielll@gmail.com>
2018-03-06 17:05:35 +0000

    More changes to utils

--- lib/PHuby/Helpers/Utils/DirUtils.php
+++ lib/PHuby/Helpers/Utils/DirUtils.php
@@ -10,4 +10,50 @@ namespace PHuby\Helpers\Utils;
 
 use PHuby\Helpers\AbstractUtils;
 
-class DirUtils extends AbstractUtils {}
+class DirUtils extends AbstractUtils {
+
+  /**
+   * @param string $path representing path of the directory to check
+   * @return boolean
+   */
+  public static function exists($path) {
+    return file_exists($path);
+  }
+
+  /**
+   * @param string $path representing path of the directory to check
+   * @return boolean
+   */
+  public static function writable($path) {
+    return is_writable($path);
+  }
+
+  /**
+   * @param string $path representing path of the directory to check
+   * @return resource|false
+   */
+  public static function open($path) {
+    if (self::exists($path)) {
+      return opendir($path);
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * @return string[] representing content of the directory
+   */
+  public static function get_content($handle) {
+    if (is_string($handle)) {
+      $handle = self::open($handle);
+    }
+    $arr_content = [];
+    while ($entry = readdir($handle)) {
+      $arr_content[] = $entry;
+    }
+    // Remove current and level up
+    $arr_content = array_diff($arr_content, ['.', '..']);
+    return $arr_content;
+  }
+
+}
